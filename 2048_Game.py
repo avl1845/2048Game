5e76fae9e70b6a4cb72bf4ea0107c6846d992469
Anders Vesterholm-Lavesen <137766103+avl1845@users.noreply.github.com>
2024-05-06 11:11:25 +0200

    Add files via upload

---
+++ 2048_Game.py
@@ -0,0 +1,127 @@
+import numpy as np
+import random
+
+board = (np.zeros(16)).reshape(4, 4)
+print(board)
+command_start = ' '
+command_up = 'w'
+command_down = 's'
+command_left = 'a'
+command_right = 'd'
+
+
+
+def add_random():
+    while True:
+        select_row = random.randint(0, 3)
+        select_column = random.randint(0, 3)
+        if board[select_row, select_column] == 0:
+            board[select_row, select_column] = 2
+            break
+        else:
+            continue
+    print(board)
+    
+def move_up():
+    for x in range(3):
+        for i in range(1, 4):
+            for j in range(4):
+                if board[i-1, j] == 0:
+                    board[i-1, j] = board[i, j]
+                    board[i, j] = 0
+    for i in range(1, 4):
+        for j in range(4):        
+            if board[i-1, j] == board[i, j]:
+                board[i-1, j] *= 2
+                board[i, j] = 0
+    for x in range(3):
+        for i in range(1, 4):
+            for j in range(4):
+                if board[i-1, j] == 0:
+                    board[i-1, j] = board[i, j]
+                    board[i, j] = 0
+    print(board)
+
+def move_down():
+    for x in range(3):
+        for i in range(2, -1, -1):
+            for j in range(4):
+                if board[i+1, j] == 0:
+                    board[i+1, j] = board[i, j]
+                    board[i, j] = 0
+    for i in range(2, -1, -1):
+        for j in range(4):        
+            if board[i+1, j] == board[i, j]:
+                board[i+1, j] *= 2
+                board[i, j] = 0
+    for x in range(3):    
+        for i in range(2, -1, -1):
+            for j in range(4):
+                if board[i+1, j] == 0:
+                    board[i+1, j] = board[i, j]
+                    board[i, j] = 0
+    print(board)
+
+def move_left():
+    for x in range(3):
+        for i in range(4):
+            for j in range(1, 4):
+                if board[i, j-1] == 0:
+                    board[i, j-1] = board[i, j]
+                    board[i, j] = 0
+    for i in range(4):
+        for j in range(1, 4):        
+            if board[i, j-1] == board[i, j]:
+                board[i, j-1] *= 2
+                board[i, j] = 0
+    for x in range(3):    
+        for i in range(4):
+            for j in range(1, 4):
+                if board[i, j-1] == 0:
+                    board[i, j-1] = board[i, j]
+                    board[i, j] = 0
+    print(board)
+
+def move_right():
+    for x in range(3):
+        for i in range(4):
+            for j in range(2, -1, -1):
+                if board[i, j+1] == 0:
+                    board[i, j+1] = board[i, j]
+                    board[i, j] = 0
+    for i in range(4):
+        for j in range(2, -1, -1):        
+            if board[i, j+1] == board[i, j]:
+                board[i, j+1] *= 2
+                board[i, j] = 0
+    for x in range(3):    
+        for i in range(4):
+            for j in range(2, -1, -1):
+                if board[i, j+1] == 0:
+                    board[i, j+1] = board[i, j]
+                    board[i, j] = 0
+    print(board)
+
+def command_center():
+    while True:
+        user_command = input("Press the command: ")
+        if user_command == command_up:
+            move_up()
+            add_random()
+        if user_command == command_down:
+            move_down()
+            add_random()
+        if user_command == command_left:
+            move_left()
+            add_random()
+        if user_command == command_right:
+            move_right()
+            add_random()
+
+
+user_command = input("Press space to start! ")
+if user_command == ' ':
+    add_random()
+    command_center()
+
+
