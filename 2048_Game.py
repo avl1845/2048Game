6a7208aa22676391ea100d0905773eab48554ade
Anders Vesterholm-Lavesen <avl@sampension.dk>
2024-05-06 13:43:50 +0200

    Added comments

--- 2048_Game.py
+++ 2048_Game.py
@@ -1,17 +1,18 @@
 import numpy as np
 import random
 
-board = (np.zeros(16)).reshape(4, 4)
+board = (np.zeros(16)).reshape(4, 4) # Creating 4x4 matrix
 print(board)
-command_start = ' '; command_up = 'w'; command_down = 's'; command_left = 'a'; command_right = 'd'
-lst_of_twos = ([2] * 17); lst_of_fours = [4, 4, 4]
-lst_total = lst_of_twos; lst_total.extend(lst_of_fours)
+command_start = ' '; command_up = 'w'; command_down = 's'; command_left = 'a'; command_right = 'd' # Making user input commands
+lst_of_twos = ([2] * 17); lst_of_fours = [4, 4, 4] # There is a 15% chance of getting a 4 after each turn. 85% chance of getting a 2.
+lst_total = lst_of_twos; lst_total.extend(lst_of_fours) # Creating list to pick from with 3 4's and 17 2's.
 
+# This function is used for adding the extra entry after each move. 
 def add_random():
     while True:
-        select_row = random.randint(0, 3)
-        select_column = random.randint(0, 3)
-        if board[select_row, select_column] == 0:
+        select_row = random.randint(0, 3) # Choosing row entry
+        select_column = random.randint(0, 3) # Choosing column entry
+        if board[select_row, select_column] == 0: # Can only add a number in an entry that is 0.
             board[select_row, select_column] = random.choice(lst_total)
             break
         else:
